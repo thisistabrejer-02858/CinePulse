c4e1e50728a9b552b75a8fae6fc5ceec538f5b0e
Tabrej Ansari <thisistabrej.er@gmail.com>
2026-04-10 19:22:57 +0530

    backend ready for render

--- Backend/server.js
+++ Backend/server.js
@@ -22,6 +22,5 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", authRoutes);
 app.use("/api/favourites", favouriteRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
